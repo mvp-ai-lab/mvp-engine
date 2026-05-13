e71adf82916326fbe07977277134e712cdfc392b
Changrui Chen <geoffreychen777@gmail.com>
2026-05-13 19:07:40 +0000

    refactor: remove unused metric imports from OpenBee utility helpers

--- recipes/openbee/utils/__init__.py
+++ recipes/openbee/utils/__init__.py
@@ -1,5 +1,1 @@
 """Utility helpers for the OpenBee recipe."""
-
-from .metrics import DistributedMetricAccumulator, MetricAccumulator
-
-__all__ = ["DistributedMetricAccumulator", "MetricAccumulator"]
